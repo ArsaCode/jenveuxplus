033250f8cbffcbf39290023212b03220b51a70c9
ArsaDevs <arsalan.ghassemi@gmail.com>
2020-09-27 06:15:09 +0200

    Added banner

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Image, ImageBackground, StyleSheet, View } from 'react-native';
+import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
 
 import colors from '../config/colors'
 
@@ -12,18 +12,15 @@ export default function WelcomeScreen(props) {
         width={200}
         height={200}
         >
-        <Image style={styles.imagelogo} source={require('../assets/logojvp.png')} />
+
+        <View style={styles.banner}>
+        <Image source={require('../assets/logojvp.png')} />
+        <Text style={styles.bannertext}>Vendez ce que vous n'utilisez plus.</Text>
+        </View>
+
         <View style={styles.buttons}>
-         <View style={styles.loginbutton} >
-            <Button 
-            title="Connexion"
-            />
-         </View>
-         <View style={styles.registerbutton} >
-            <Button 
-            title="Inscription"
-            />
-         </View>
+         <View style={styles.loginbutton} />
+         <View style={styles.registerbutton} />
         </View>
         </ImageBackground>
     );
@@ -34,14 +31,27 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
     },
+    banner: {
+        alignItems: 'center',
+    },
+    bannertext: {
+        top: 0,
+        fontSize: 20,
+    },
     buttons: {
         position: 'absolute',
+        width: '100%',
         bottom: 50,
     },
     loginbutton: {
+        width: '100%',
+        height: 70,
         backgroundColor: colors.primary,
     },
     registerbutton: {
+        marginTop: 20,
+        width: '100%',
+        height: 70,
         backgroundColor: colors.primary,
     }
 })

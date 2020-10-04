ef4e4be78f5bef844245cb44bcb2b0c296f34606
Arsalan Ghassemi <contact@arsadevs.com>
2020-10-04 16:13:16 +0200

    Added custom text input

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
-import AccountScreen from './app/screens/AccountScreen';
+import AppTextInput from './app/components/AppTextInput';
 
 export default function App() {
   return (
-    <AccountScreen />
+    <AppTextInput
+    icon="form"
+    placeholder="Votre prénom"
+    />
   );
 }
---
+++ app/components/AppTextInput.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StyleSheet, TextInput, View } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+
+import colors from '../config/colors'
+
+export default function AppTextInput({icon, placeholder}) {
+    return (
+        <View style={styles.container}>
+            {icon && <AntDesign 
+            name={icon}
+            size={24}
+            color="black"
+            style={styles.icon}
+            />}
+            <TextInput 
+            style={styles.textInput}
+            placeholder={placeholder}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignSelf: 'center',
+        backgroundColor: colors.white2,
+        flexDirection: 'row',
+        borderRadius: 25,
+        width: '95%',
+        padding: 15,
+        marginVertical: 10,
+    },
+    icon: {
+        marginRight: 10,
+    },
+    textInput: {
+        fontSize: 18,
+        fontFamily: Platform.OS === "android" ? 'Roboto' : 'Avenir',
+    }
+})
